5e4bffd48b3d4223eeffd1ec401ff8a863caaa4f
jkpark512 <43334484+jkpark512@users.noreply.github.com>
2018-09-20 15:57:16 +0900

    구직자 자바 파일 수정 및 뷰페이지 만듬

--- src/member/jobseeker/JobSeekerDBBean.java
+++ src/member/jobseeker/JobSeekerDBBean.java
@@ -12,10 +12,26 @@ public class JobSeekerDBBean {
 		return rst; // 에러막기위한 임시 리턴. 필요없으면 지울것.
 	}
 	
-	public int jbskLgn(String iptId, String iptPasswd) { // 임시매개변수. 필요시 변경할것.
-		int rst=0;
-		return rst; // 에러막기위한 임시 리턴. 필요없으면 지울것.
+	public int jbskLgn(String jbskId, String jbskPasswd) { // 임시매개변수. 필요시 변경할것.
+		int result = 0;
+		
+		if(check(jbskId)>0) {
+			// 아이디가 있다
+			JobSeekerDataBean jbskDto = jbskGet(jbskId);
+			if( jbskPasswd.equals( jbskDto.getJbskPasswd() ) ) {
+				//비밀번호 같다
+				result = 1;
+			} else {
+				//비밀번호 다르다
+				result = -1;
+			}				
+		} else {
+			// 아이디가 없다
+			result = 0;				
+		}
+		return result;
 	}
+	
 	
 	public JobSeekerDataBean jbskGet(String jbskId) { // 임시매개변수. 필요시 변경할것.
 		JobSeekerDataBean jbskDto = new JobSeekerDataBean();
