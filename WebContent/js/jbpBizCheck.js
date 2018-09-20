a70de977f45a30bd136149f518dee20c2757facc
leoh7 <leoh7@naver.com>
2018-09-20 16:51:17 +0900

    jbpReg 에 맞게 변수수정

--- WebContent/js/jbpBizCheck.js
+++ WebContent/js/jbpBizCheck.js
@@ -1,14 +1,18 @@
+/*
+ * 사업자 등록번호 유효성검사
+ * 
+ */
+
 $(document).ready(
 	function (){
 		$('input:button[name=checkBizID]').on(
 			'click',
 			function checkBizID() {
-				if( !$('input:text[name=jbpNumber]').val() ){
+				if( !$('input:text[name=jobpBno]').val() ){
 					alert("사업자번호를 입력해주세요");
 					return false;
 				} else {
-					alert("체크");
-					var bizID = $('input:text[name=jbpNumber]').val();
+					var bizID = $('input:text[name=jobpBno]').val();
 				    var checkID = new Array(1, 3, 7, 1, 3, 7, 1, 3, 5, 1);
 				    var tmpBizID, i, chkSum=0, c2, remander;
 				    var result;
@@ -26,6 +30,11 @@ $(document).ready(
 				 
 				    if (Math.floor(bizID.charAt(9)) == remander) {
 				    	alert('사업자등록번호 형식에 맞는 번호입니다');
+				    	// TODO : 맞는 사업자 등록번호일 시, bizID 앞 6자리 파싱해서 공공데이터 포털날리고
+				    	// 폼 만들어서 선택하게끔
+				    	var bizName = $('input:text[name=jobpCn]').val();
+				    	var parseBizID = bizID.substring(0, 6);
+				    	alert(parseBizID)
 				    	result = true ; // OK!
 				    } else {
 				    	alert('사업자등록번호 형식에 맞지 않습니다');
