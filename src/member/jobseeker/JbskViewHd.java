93ef0e2e6c2eacba566a28bd675885d1b36ff33e
hyewon3701 <hyewon3701@users.noreply.github.com>
2018-09-21 17:59:37 +0900

    delete .classpath....

--- src/member/jobseeker/JbskViewHd.java
+++ src/member/jobseeker/JbskViewHd.java
@@ -18,7 +18,7 @@ public class JbskViewHd implements JobSeekerHandler {
 	private JobSeekerDBBean jbskDao; 
 	@Override
 	@RequestMapping("/jbskView") 
-	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws JbskException {
+	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws JobSeekerException {
 		
 			String jbskId = (String) request.getSession().getAttribute( "memid" );
 			JobSeekerDataBean jbpDto = jbskDao.jbskGet(jbskId);
