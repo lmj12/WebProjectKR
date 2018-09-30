ca4db4966b4df1ab0f46ac26a1d3896fecb3c580
leoh7 <leoh7@naver.com>
2018-09-30 12:14:19 +0900

    입력경력사항 DBBean / iptCheck 단계

--- src/iptcareer/IptCrrHd.java
+++ src/iptcareer/IptCrrHd.java
@@ -11,16 +11,22 @@ import org.springframework.web.servlet.ModelAndView;
 public class IptCrrHd implements InputCareerHandler {
 
 	@Resource
-	private IptCrrDBBean iptCrrDBBean;
+	private IptCrrDBBean iptCrr;					// 입력경력사항 DBBean
 	
 	@Override
 	@RequestMapping("/iptCrr")
 	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws IptCrrException {
-		// TODO : 입력경력사항 리스트
+		// TODO : 입력경력사항 종합화면
 		
-		// FIXME : setSession 된 아이디 아이디
-		
-		return new ModelAndView("");
-	}
+		String memid = "aaa";	// FIXME : setSession 된 아이디 아이디 임시
+		int iptListCnt = iptCrr.iptCheck(memid);
 
+		if(iptListCnt == 0){
+			request.setAttribute("iptListCnt", iptListCnt);	// 입력한 내용 없을 때
+		} else {
+			request.setAttribute("iptListCnt", iptListCnt);	// 입력한 내용 있을 때
+			
+		}
+		return new ModelAndView("iptCrr/iptCrr");
+	}
 }
