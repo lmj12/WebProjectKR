27de11e3f8c77f0e306c064039847f2d5fb8b319
leoh7 <leoh7@naver.com>
2018-09-30 12:12:40 +0900

    iptCrr.do 로 입력경력사항 페이지 이동 (iptCrr/iptCrr.jsp)

---
+++ WebContent/iptCrr/iptCrr.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<h2> 입력경력사항 </h2>
+
+체크 후 입력경력사항 있으면 / 내용 출력, 수정 버튼, 삭제 버튼
+				없으면 / 입력경력이 없다 출력 ,입력경력사항 쓰기 버튼
+
+<table>
+	<thead>
+		
+	</thead>
+	<tbody>
+	
+	</tbody>
+</table>
---
+++ src/move/IptCrrMove.java
@@ -0,0 +1,19 @@
+package move;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IptCrrMove implements MoveHandler {
+
+	@Override
+	@RequestMapping("/iptCrr")
+	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws MoveException {
+		// TODO : move 입력경력사항 List 기본
+		return new ModelAndView("iptCrr/iptCrr");
+	}
+}
