cff141f88c9eb502664da8e85515f2fd67e56ecb
leoh7 <leoh7@naver.com>
2018-10-02 17:29:46 +0900

    필요없는 내용 정리 및 입력경력사항 없을 때, 개인정보도 못 불러오던 부분 수정

--- src/iptcareer/IptCrrHd.java
+++ src/iptcareer/IptCrrHd.java
@@ -24,21 +24,14 @@ public class IptCrrHd implements InputCareerHandler {
 	@RequestMapping("/iptCrr")
 	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws IptCrrException {
 		// TODO : 입력경력사항 종합화면
-		String jbskId = (String) request.getSession().getAttribute("memid");	// FIXME : setSession 에 있는 아이디 아이디 임시로 주입
+		String jbskId = (String) request.getSession().getAttribute("memid");
 		int iptListCnt = iptCrrDao.iptCheck(jbskId);
 		JobSeekerDataBean jbskDto = jbskDao.jbskGet(jbskId);
+		request.setAttribute("iptListCnt", iptListCnt);	
 		
-		if(iptListCnt == 0){
-			request.setAttribute("iptListCnt", iptListCnt);	// 입력한 내용 없을 때
-		} else {
-			request.setAttribute("iptListCnt", iptListCnt);	// 입력한 내용 있을 때
-			
 			///////////////// 구직자 기본정보 사항 관련 /////////////////
-			request.setAttribute("jbskName", jbskDto.getJbskName());		// 이름
-			request.setAttribute("jbskBd", jbskDto.getJbskBd());			// 생년월일
-			request.setAttribute("jbskGender", jbskDto.getJbskGender());	// 성별
-			request.setAttribute("jbskTel", jbskDto.getJbskTel());			// 연락처
-			String address = jbskDto.getJbskAdd1()+jbskDto.getJbskAdd2()+jbskDto.getJbskAdd3();
+			request.setAttribute("jbskDto", jbskDto);
+			String address = jbskDto.getJbskAdd1()+" "+jbskDto.getJbskAdd2()+" "+jbskDto.getJbskAdd3();
 			request.setAttribute("jbskAddress", address);					// 주소
 			
 			///////////////// 구직자 recCrr 관련 /////////////////
@@ -47,7 +40,7 @@ public class IptCrrHd implements InputCareerHandler {
 			// TODO : 자바에서 Date 관련 TimeStamp 로 처리하기 
 			List<IptCrrDataBean> iptDtoList = iptCrrDao.iptList(jbskId);
 			request.setAttribute("iptDtoList", iptDtoList);
-		}
+			
 		return new ModelAndView("iptCrr/iptCrr");
 	}
 }
