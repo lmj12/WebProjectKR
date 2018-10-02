578eafe8378c9ce9d24075c5f16df71863ae61f3
leoh7 <leoh7@naver.com>
2018-10-02 22:05:20 +0900

    필요없는 주석들 정리

--- src/iptcareer/IptCrrHd.java
+++ src/iptcareer/IptCrrHd.java
@@ -25,21 +25,22 @@ public class IptCrrHd implements InputCareerHandler {
 	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws IptCrrException {
 		// TODO : 입력경력사항 종합화면
 		String jbskId = (String) request.getSession().getAttribute("memid");
-		int iptListCnt = iptCrrDao.iptCheck(jbskId);
 		JobSeekerDataBean jbskDto = jbskDao.jbskGet(jbskId);
-		request.setAttribute("iptListCnt", iptListCnt);	
+		int iptListCnt = iptCrrDao.iptCheck(jbskId);
 		
-			///////////////// 구직자 기본정보 사항 관련 /////////////////
-			request.setAttribute("jbskDto", jbskDto);
-			String address = jbskDto.getJbskAdd1()+" "+jbskDto.getJbskAdd2()+" "+jbskDto.getJbskAdd3();
-			request.setAttribute("jbskAddress", address);					// 주소
-			
-			///////////////// 구직자 recCrr 관련 /////////////////
-			
-			///////////////// 구직자 iptCrr 관련 /////////////////
-			// TODO : 자바에서 Date 관련 TimeStamp 로 처리하기 
-			List<IptCrrDataBean> iptDtoList = iptCrrDao.iptList(jbskId);
-			request.setAttribute("iptDtoList", iptDtoList);
+		request.setAttribute("iptListCnt", iptListCnt);
+	
+		///////////////// 구직자 기본정보 사항 관련 /////////////////
+		request.setAttribute("jbskDto", jbskDto);
+		String address = jbskDto.getJbskAdd1()+" "+jbskDto.getJbskAdd2()+" "+jbskDto.getJbskAdd3();
+		request.setAttribute("jbskAddress", address);					
+		
+		///////////////// 구직자 recCrr 관련 /////////////////
+		
+		///////////////// 구직자 iptCrr 관련 /////////////////
+		// TODO : 자바에서 Date 관련 TimeStamp 로 처리하기 
+		List<IptCrrDataBean> iptDtoList = iptCrrDao.iptList(jbskId);
+		request.setAttribute("iptDtoList", iptDtoList);
 			
 		return new ModelAndView("iptCrr/iptCrr");
 	}
