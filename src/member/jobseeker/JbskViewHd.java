08d717239930fc05d97cd1780ca460db7ae07799
lmj12 <43236802+lmj12@users.noreply.github.com>
2018-10-04 13:20:04 +0900

    팀 작업관련 코드들 및 구직자 로그인시 팀가져오는 기능 구현

--- src/member/jobseeker/JbskViewHd.java
+++ src/member/jobseeker/JbskViewHd.java
@@ -1,14 +1,22 @@
 package member.jobseeker;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import member.jobseeker.JobSeekerException;
+import schedule.job.SchJbException;
 import member.jobseeker.JobSeekerDBBean;
 import member.jobseeker.JobSeekerDataBean;
 
@@ -29,5 +37,26 @@ public class JbskViewHd implements JobSeekerHandler {
 			return new ModelAndView("/Jbs/jbskMod");
 		
 	}
+	
+	@RequestMapping(value = "ajaxJbskGet") 
+	@ResponseBody
+	public String ajaxProcess(HttpServletRequest request, HttpServletResponse response) throws SchJbException {
+		String jbskId =  request.getParameter("jbskId");
+		JobSeekerDataBean jbskDto = jbskDao.jbskGet(jbskId);
+		
+		
+		ObjectMapper mapper = new ObjectMapper(); 
+		
+		String jbsk=""; 
+		try { 
+			jbsk = mapper.writeValueAsString(jbskDto);
+			
+		} catch (IOException e) { 
+			e.printStackTrace(); 
+		}
+		
+		return jbsk;
+	}
+
 
 }
--- src/team/TeamReqCk.java
+++ src/team/TeamReqCk.java
@@ -12,28 +12,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Controller
-public class TeamListHd implements TeamHandler {
+public class TeamReqCk implements TeamHandler {
 	@Resource
 	private TeamDBBean teamDao;
 	@Override
-	@RequestMapping("/teamList")
+	@RequestMapping("teamReqCk")
 	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws TeamException {
-		int teamId = (int) request.getSession().getAttribute("teamId");	//	jsp세션에 저장된 팀 id받아오기.
-		List<TeamDataBean> teamlist = teamDao.teamList(teamId);
+		String jbskId = (String) request.getSession().getAttribute("memid");
+		
+		System.out.println(jbskId);
+		List<TeamDataBean> reqTeams = teamDao.ckReq(jbskId);
 		
 		ObjectMapper mapper = new ObjectMapper(); 
 		
 		String tl=""; 
 		try { 
-			tl = mapper.writeValueAsString(teamlist);
+			tl = mapper.writeValueAsString(reqTeams);
 			
 		} catch (IOException e) { 
 			e.printStackTrace(); 
 		}
 		
-		request.setAttribute("teamlist", tl);
-		return new ModelAndView("team/teamView");
+		request.setAttribute("reqTeams", tl);
+		
+		return new ModelAndView("team/teamReqCk");
 	}
 
 }
