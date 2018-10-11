8cea84bdb9e6fecfe0c35e04c0a3478240b3086c
lmj12 <43236802+lmj12@users.noreply.github.com>
2018-10-11 11:52:26 +0900

    공고 지원 완료
    추후 스케줄과 연동 고려사항.

--- src/reccareer/RecCrrMyViewHd.java
+++ src/reccareer/RecCrrMyViewHd.java
@@ -1,5 +1,8 @@
 package reccareer;
 
+import java.util.List;
+
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,11 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class RecCrrMyViewHd implements RecruitCareerHandler {
-
+	@Resource
+	private RecCrrDBBean recCrrDao;
 	@Override
-	@RequestMapping("/recCrrMyView")
+	@RequestMapping("/recCrrMy")
 	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws RecCrrException {
-		return null;
+		String jbskId = (String) request.getSession().getAttribute("memid");
+		
+		List<RecCrrDataBean> recCrrlists = recCrrDao.recCrrMyList(jbskId);
+		request.setAttribute("recCrrlists", recCrrlists);
+		
+		List<RecCrrDataBean> recCrrPrevlists = recCrrDao.recCrrMyPrevList(jbskId);
+		request.setAttribute("recCrrPrevlists", recCrrPrevlists);
+		
+		return new ModelAndView("/recCrr/recCrrMy");
 	}
 
 }
