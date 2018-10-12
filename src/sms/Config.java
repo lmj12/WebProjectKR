a12dd0356661ae10ea53b137e2f526211bb8abfe
lmj12 <43236802+lmj12@users.noreply.github.com>
2018-10-12 10:06:04 +0900

    SMS테스트용 임시머지.

---
+++ src/sms/Config.java
@@ -0,0 +1,9 @@
+package sms;
+
+public final class Config {
+	public static final String appid = "wdjoa";
+	public static final String apikey = "c24dc790cdb711e8b3210cc47a1fcfae";
+	public static final String content = "나는 유리를 먹을 수 있어요. 그래도 아프지 않아요";
+	public static final String sender = "01067878012";
+	public static final String receiver = "01067878012";
+}
---
+++ src/sms/Test.java
@@ -0,0 +1,10 @@
+package sms;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SendSMS sms = new SendSMS();
+	}
+
+}
