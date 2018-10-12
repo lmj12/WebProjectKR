c548b0289156e7a1a27235074740b031136bf43b
leoh7 <leoh7@naver.com>
2018-10-12 21:52:54 +0900

    퇴근용 커밋

--- WebContent/js/jbskReg.js
+++ WebContent/js/jbskReg.js
@@ -2,7 +2,6 @@
 // Daum Api 주소 검색 
 
 function searchPostcode() {
-	var string = String(value)
     new daum.Postcode({
         oncomplete: function(data) {
             // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
